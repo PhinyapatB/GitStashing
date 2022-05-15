d45bd2570f0c8b22a3ef47998ea3b2f9b7663bc1
PhinyapatB <phin.b12@gmail.com>
2022-05-15 12:07:16 -0400

    The red car is ready with some basic features

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        basicFeatures car = new basicFeatures();
+        basicFeatures Auddi = new basicFeatures();
+        Auddi.color="red";
+        Auddi.doors="4";
+        Auddi.engineCapac="V8";
+        Auddi.headlights="blue";
+        Auddi.regNo="tuzik";
     }
 }
